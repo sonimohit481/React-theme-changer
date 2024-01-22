c3dcda341c6e492e76e2ee6ee2ddec23fd9f28a7
Mohit Soni <sonimohit481@gmail.com>
2024-01-22 11:27:33 +0530

    Theme context improvised

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,31 +1,32 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 
-export const ThemeContext = createContext({
+const ThemeContext = createContext({
   themeMode: "light",
   darkTheme: () => {},
   lightTheme: () => {},
 });
-const [themeMode, setThemeMode] = useState("light");
-
-const lightTheme = () => {
-  setThemeMode("light");
-};
-
-const darkTheme = () => {
-  setThemeMode("dark");
-};
-
-useEffect(() => {
-  const htmlElement = document.querySelector("html");
-  htmlElement?.classList.remove("light", "dark");
-  htmlElement?.classList.add(themeMode);
-}, [themeMode]);
 
 interface ThemeProviderProps {
   children: React.ReactNode;
 }
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    const htmlElement = document.querySelector("html");
+    htmlElement?.classList.remove("light", "dark");
+    htmlElement?.classList.add(themeMode);
+  }, [themeMode]);
+
   return (
     <ThemeContext.Provider value={{ themeMode, lightTheme, darkTheme }}>
       {children}
