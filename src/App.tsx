01c741353bcf1ff84e371eaaf49e60a449e1fada
Mohit Soni <sonimohit481@gmail.com>
2024-01-22 11:33:09 +0530

    Context applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
+
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <ThemeBtn />
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          <Card />
+        </div>
+      </div>
+    </div>
   );
 }
 
